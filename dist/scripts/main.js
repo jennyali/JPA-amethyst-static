85d0fa690283c764f8a8423f4d0006fe6dca01b0
Jennifer Ali <Jennifer Ali>
2017-04-25 11:09:25 +0100

    update

--- dist/scripts/main.js
+++ dist/scripts/main.js
@@ -1,10 +1,28 @@
 $(document).ready(function() {
 
-$('.jumbotron').on({
-    'click': function () {
-        $(this).fadeOut();
-    }
-});
 
+// True parallax EFFECT, bg scolls slowly slightly up
+
+    // cache the window object
+    var $window = $(window);
+
+    //parallax background effect
+    $('section[data-type="background"]').each(function() {
+
+        var $bgobj = $(this); // assigning the object
+
+        $(window).scroll(function() {
+
+            // scroll the background at var speed
+            // the yPos is a negative value because we're scrolling up
+
+            var yPos = -($window.scrollTop() / $bgobj.data('speed'));
+
+            var coords = '50%' + yPos + 'px';
+
+            $bgobj.css({ 'background-position' : coords });
+
+        });
+    });
 
 });
